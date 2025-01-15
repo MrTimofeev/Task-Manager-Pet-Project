de401b1b8dee86f6b1dcb4e8bf1cb2a6719fdb71
MrTimofeev <nemo.capitan.26ru@mail.ru>
2025-01-15 16:04:15 +0300

    Добавления настроек для

--- task_manager/task_manager/settings.py
+++ task_manager/task_manager/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-@%u(rq7#!wg$+i48#8!4+451ty7w*d$ho#ogt($-a6umbtsyl*"
+SECRET_KEY = os.getenv('SECRET_KEY', 'your-default-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']  # Разрешает все хосты (для тестирования)
 
 
 # Application definition
